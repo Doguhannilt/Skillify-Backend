9d4de673589b0be03c35339600f61a90f6d469b0
doguhannilt <doguhannilt@gmail.com>
2025-01-06 21:57:35 +0300

    response updated

---
+++ src/main/java/com/skillify/project/response/AuthResponse.java
@@ -0,0 +1,33 @@
+package com.skillify.project.response;
+
+
+public class AuthResponse {
+    private String jwt;
+    private String message;
+    private String role;
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
