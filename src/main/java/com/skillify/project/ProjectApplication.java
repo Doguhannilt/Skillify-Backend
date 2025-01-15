73c8a674f51455fcde9d42a2c6e2e5f68c462e54
doguhannilt <doguhannilt@gmail.com>
2025-01-15 07:23:13 +0300

    ComponentScan added

--- src/main/java/com/skillify/project/ProjectApplication.java
+++ src/main/java/com/skillify/project/ProjectApplication.java
@@ -3,9 +3,11 @@ package com.skillify.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
+@ComponentScan(basePackages = {"com.skillify.project"})
 public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
